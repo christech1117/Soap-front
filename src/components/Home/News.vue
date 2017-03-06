ba65ea026284ef26d67f5c1b75023240246e2bd8
username <user email>
2017-03-06 15:32:12 +0800

    modify footer && add news

---
+++ src/components/Home/News.vue
@@ -0,0 +1,66 @@
+<template lang="html">
+  <!-- Latest work -->
+  <div class="work-container">
+    <div class="container">
+      <div class="row">
+          <div class="col-sm-12 work-title wow fadeIn">
+              <h2>Our Latest Work</h2>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm-3">
+              <div class="work wow fadeInUp">
+                  <img src="static/images/portfolio/work1.jpg" alt="Lorem Website" data-at2x="static/images/portfolio/work1.jpg">
+                  <h3>Lorem Website</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
+                  <div class="work-bottom">
+                      <a class="big-link-2 view-work" href="static/images/portfolio/work1.jpg"><i class="fa fa-search"></i></a>
+                      <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
+                  </div>
+              </div>
+            </div>
+            <div class="col-sm-3">
+              <div class="work wow fadeInDown">
+                  <img src="static/images/portfolio/work2.jpg" alt="Ipsum Logo" data-at2x="static/images/portfolio/work2.jpg">
+                  <h3>Ipsum Logo</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
+                  <div class="work-bottom">
+                      <a class="big-link-2 view-work" href="static/images/portfolio/work2.jpg"><i class="fa fa-search"></i></a>
+                      <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
+                  </div>
+              </div>
+          </div>
+          <div class="col-sm-3">
+              <div class="work wow fadeInUp">
+                  <img src="static/images/portfolio/work3.jpg" alt="Dolor Prints" data-at2x="static/images/portfolio/work3.jpg">
+                  <h3>Dolor Prints</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
+                  <div class="work-bottom">
+                      <a class="big-link-2 view-work" href="static/images/portfolio/work3.jpg"><i class="fa fa-search"></i></a>
+                      <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
+                  </div>
+              </div>
+          </div>
+          <div class="col-sm-3">
+              <div class="work wow fadeInDown">
+                  <img src="static/images/portfolio/work4.jpg" alt="Sit Amet Website" data-at2x="static/images/portfolio/work4.jpg">
+                  <h3>Sit Amet Website</h3>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor...</p>
+                  <div class="work-bottom">
+                      <a class="big-link-2 view-work" href="static/images/portfolio/work4.jpg"><i class="fa fa-search"></i></a>
+                      <a class="big-link-2" href="portfolio.html"><i class="fa fa-link"></i></a>
+                  </div>
+              </div>
+          </div>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
--- src/components/Home/index.vue
+++ src/components/Home/index.vue
@@ -2,15 +2,18 @@
   <div class="Home">
     <Slider></Slider>
     <Hot-product></Hot-product>
+    <br>
+    <News></News>
   </div>
 </template>
 
 <script>
 import Slider from './Slider'
 import HotProduct from './HotProduct'
+import News from './News'
 
 export default {
-  components: { Slider, HotProduct }
+  components: { Slider, HotProduct, News }
 }
 </script>
 
