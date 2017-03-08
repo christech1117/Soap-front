38356ff6e22dc97344dc63ec2c9fad32b6ce81fb
username <user email>
2017-03-08 10:25:25 +0800

    Modify header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -11,43 +11,41 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <router-link class="navbar-brand" :to="{ name: 'Home' }">Andia - a super cool design agency...</router-link>
+          <a class="navbar-brand" href="http://localhost:8080/">Andia - a super cool design agency...</a>
         </div>
         <!-- Collect the nav links, forms, and other content for toggling -->
         <div class="collapse navbar-collapse" id="top-navbar-1">
           <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown active">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="1000">
-                <i class="fa fa-home"></i><br>Home <span class="caret"></span>
-              </a>
-              <ul class="dropdown-menu dropdown-menu-left" role="menu">
-                <li class="active"><a href="index.html">Home</a></li>
-                <li><a href="index-2.html">Home 2</a></li>
-              </ul>
+            <li>
+              <a href="http://localhost:8080/"><i class="fa fa-home"></i><br>首頁</a>
             </li>
             <li>
-              <a href="portfolio.html"><i class="fa fa-camera"></i><br>會員登入</a>
+              <a href="http://localhost:8080/news"><i class="fa fa-newspaper-o"></i><br>最新消息</a>
             </li>
             <li>
-              <a href="services.html"><i class="fa fa-tasks"></i><br>最新消息</a>
-            </li>
-            <li>
-              <a href="Product"><i class="fa fa-envelope"></i><br>商品介紹</a>
+              <a href="http://localhost:8080/product"><i class="fa fa-tasks"></i><br>商品介紹</a>
             </li>
             <li class="dropdown">
               <a href="#" class="dropdown-toggle" data-toggle="dropdown" data-hover="dropdown" data-delay="1000">
                 <i class="fa fa-paperclip"></i><br>購物須知 <span class="caret"></span>
               </a>
               <ul class="dropdown-menu" role="menu">
-                <li><a href="pricing-tables.html">如何購買</a></li>
+                <li><a href="http://localhost:8080/">如何購買</a></li>
+                <li><a href="http://localhost:8080/">付款方式</a></li>
+                <li><a href="http://localhost:8080/">如何退貨</a></li>
               </ul>
             </li>
             <li>
-              <a href="about.html"><i class="fa fa-user"></i><br>關於我們</a>
+              <a href="http://localhost:8080/about"><i class="fa fa-user"></i><br>關於我們</a>
             </li>
             <li>
-              <router-link :to="{ name: 'Contact' }"><i class="fa fa-envelope"></i><br>聯絡我們</router-link>
-              <!-- <a href="contact.html"><i class="fa fa-envelope"></i><br>聯絡我們</a> -->
+              <a href="http://localhost:8080/contact"><i class="fa fa-envelope"></i><br>聯絡我們</a>
+            </li>
+            <li>
+              <a href="http://localhost:8080/"><i class="fa fa-sign-in"></i><br>會員登入</a>
+            </li>
+            <li>
+              <a href="http://localhost:8080/cart"><i class="fa fa-shopping-cart"></i><br>購物車</a>
             </li>
           </ul>
         </div>
@@ -62,5 +60,4 @@ export default {
 </script>
 
 <style scoped>
-
 </style>
