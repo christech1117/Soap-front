ef08acf428ac5c3abec17f1d49f22fe131efd355
username <user email>
2017-03-08 15:56:07 +0800

    Delete additional code

--- src/components/Login/Login.vue
+++ src/components/Login/Login.vue
@@ -1,28 +1,25 @@
 <template lang="html">
   <div class="Login">
-    <div class="Contact">
-      <!-- Contact Us -->
-      <div class="contact-us-container">
-        <div class="container">
-            <span><h1>會員登入</h1></span>
-            <div class="row">
-                <div class="col-sm-6 contact-form wow fadeInLeft">
-                    <form role="form" action="" method="">
-                      <div class="form-group">
-                        <label for="contact-name">帳號</label>
-                        <input type="text" name="name" placeholder="Enter your account..." class="contact-name" id="contact-name">
-                      </div>
-                      <div class="form-group">
-                        <label for="contact-password">密碼</label>
-                        <input type="password" name="password" placeholder="Enter your password..." class="contact-password" id="contact-password">
-                      </div>
-                        <button type="submit" class="btn">登入</button>
-                        <button type="submit" class="btn">忘記密碼</button>
-                        <button type="submit" class="btn">註冊</button>
-                    </form>
-                </div>
-            </div>
-        </div>
+    <div class="contact-us-container">
+      <div class="container">
+          <span><h1>會員登入</h1></span>
+          <div class="row">
+              <div class="col-sm-6 contact-form wow fadeInLeft">
+                  <form role="form" action="" method="">
+                    <div class="form-group">
+                      <label for="contact-name">帳號</label>
+                      <input type="text" name="name" placeholder="Enter your account..." class="contact-name" id="contact-name">
+                    </div>
+                    <div class="form-group">
+                      <label for="contact-password">密碼</label>
+                      <input type="password" name="password" placeholder="Enter your password..." class="contact-password" id="contact-password">
+                    </div>
+                      <button type="submit" class="btn">登入</button>
+                      <button type="submit" class="btn">忘記密碼</button>
+                      <button type="submit" class="btn">註冊</button>
+                  </form>
+              </div>
+          </div>
       </div>
     </div>
   </div>
@@ -34,4 +31,5 @@ export default {
 </script>
 
 <style scoped>
+
 </style>
