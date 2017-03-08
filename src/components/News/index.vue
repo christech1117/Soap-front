9c75c9b5e1b9bee9e9f19cefce87b725e6b2dbbc
username <user email>
2017-03-08 10:25:14 +0800

    add news && about us

---
+++ src/components/News/index.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <div class="News">
+    <!-- Page Title -->
+    <div class="page-title-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 wow fadeIn">
+                    <i class="fa fa-envelope"></i>
+                    <h1>Home / News</h1>
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <News></News>
+    <News></News>
+    <News></News>
+    <News></News>
+    <News></News>
+  </div>
+</template>
+
+<script>
+import News from '../Home/News'
+
+export default {
+  components: { News }
+}
+</script>
+
+<style scoped>
+</style>
