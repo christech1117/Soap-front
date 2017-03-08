155a320aa83a9b9a9ac67fc64ce89bf57ad61c6a
username <user email>
2017-03-08 15:30:45 +0800

    Add login && forget pass .vue

---
+++ src/components/Login/Login.vue
@@ -0,0 +1,37 @@
+<template lang="html">
+  <div class="Login">
+    <div class="Contact">
+      <!-- Contact Us -->
+      <div class="contact-us-container">
+        <div class="container">
+            <span><h1>會員登入</h1></span>
+            <div class="row">
+                <div class="col-sm-6 contact-form wow fadeInLeft">
+                    <form role="form" action="" method="">
+                      <div class="form-group">
+                        <label for="contact-name">帳號</label>
+                        <input type="text" name="name" placeholder="Enter your account..." class="contact-name" id="contact-name">
+                      </div>
+                      <div class="form-group">
+                        <label for="contact-email">密碼</label>
+                        <input type="password" name="email" placeholder="Enter your password..." class="contact-email" id="contact-email">
+                      </div>
+                        <button type="submit" class="btn">登入</button>
+                        <button type="submit" class="btn">忘記密碼</button>
+                        <button type="submit" class="btn">註冊</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/Login/index.vue
@@ -0,0 +1,28 @@
+<template lang="html">
+  <div class="Login">
+    <!-- Page Title -->
+    <div class="page-title-container">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 wow fadeIn">
+                    <i class="fa fa-envelope"></i>
+                    <h1>Home / Login</h1>
+                    <!-- <p>Here is how you can contact us</p> -->
+                </div>
+            </div>
+        </div>
+    </div>
+    <Login></Login>
+  </div>
+</template>
+
+<script>
+import Login from './Login'
+
+export default {
+  components: { Login }
+}
+</script>
+
+<style scoped>
+</style>
