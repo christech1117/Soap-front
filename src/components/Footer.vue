badb6aed6c806aede1839bf01460d609c85bed46
username <user email>
2017-03-08 10:25:32 +0800

    modify footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,18 @@
 <template lang="html">
-  <!-- Footer -->
-  <div class="Footer">
-
-  <footer>
-      <div class="container">
-          <div class="row">
-              <div class="col-sm-7 footer-copyright wow fadeIn">
-                  <p>Copyright 2017 Chris - All Rights Reserved.</a>.</p>
-              </div>
-              <div class="col-sm-5 footer-social wow fadeIn">
-                  <a href="#"><i class="fa fa-facebook"></i></a>
-                  <a href="#"><i class="fa fa-dribbble"></i></a>
-                  <a href="#"><i class="fa fa-twitter"></i></a>
-                  <a href="#"><i class="fa fa-pinterest"></i></a>
-              </div>
-          </div>
+  <div class="panel-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col-sm-7 footer-copyright wow fadeIn">
+              <p>Copyright 2017 Chris - All Rights Reserved.</a>.</p>
+        </div>
+        <div class="col-sm-5 footer-social wow fadeIn">
+              <a href="#"><i class="fa fa-facebook"></i></a>
+              <a href="#"><i class="fa fa-dribbble"></i></a>
+              <a href="#"><i class="fa fa-twitter"></i></a>
+              <a href="#"><i class="fa fa-pinterest"></i></a>
+        </div>
       </div>
-  </footer>
+    </div>
   </div>
 </template>
 
@@ -26,4 +22,9 @@ export default {
 </script>
 
 <style scoped>
+.panel-footer {
+  background-image: linear-gradient(120deg, #fdfbfb 0%, #ebedee 100%);
+}
+
+
 </style>
