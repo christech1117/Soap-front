8736553c47114ebbc7fd79448794edcc7ad753d2
username <user email>
2017-04-28 17:58:41 +0800

    修改card

--- src/components/Home/News.vue
+++ src/components/Home/News.vue
@@ -3,29 +3,18 @@
     <div class="row">
       <div class="col-sm-4">
         <div class="card wow fadeInUp">
-          <!-- <div class="overlay"></div> -->
-          <div class="card-img">
-            <a href="">
-              <h3>結婚不知如何開始準備嗎？</h3>
-              <p>【海外訂購服務開啟】</p>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div class="col-sm-4">
-        <div class="card wow fadeInUp">
           <a href="">
-            <img src="https://www.teasoap.com.tw/downloads/bbs/pic/20170301115046.jpg" alt="Ipsum Logo" data-at2x="static/images/portfolio/card2.jpg">
-            <h3>2017/10/21</h3>
-            <p>【10/21～10/30暫停退貨退款作業】</p>
+            <img src="http://www.monga.com.tw/NewsImgs/74/2016%E8%87%BA%E7%81%A3%E6%96%87%E5%8D%9A%E6%9C%83%20%E8%89%8B%E8%88%BA%E8%82%A5%E7%9A%825.jpg" alt="Ipsum Logo" data-at2x="static/images/portfolio/card2.jpg">
+            <h4>結婚不知如何開始準備嗎？</h4>
+            <p>一個悶熱準備下起雷陣雨的午后，計程車穿越過快速道路，原以為這趟採訪路途遙遠，沒想到約15分鐘下交流道來到了新店，這次要採訪的是「京采...</p>
           </a>
         </div>
       </div>
       <div class="col-sm-4">
         <div class="card wow fadeInUp">
           <a href="">
-            <img src="https://www.teasoap.com.tw/downloads/bbs/pic/20170301133344.jpg" alt="Dolor Prints" data-at2x="static/images/portfolio/card3.jpg">
-            <h3>2017/11/17</h3>
+            <img src="http://www.monga.com.tw/NewsImgs/79/3.jpg" alt="Dolor Prints" data-at2x="static/images/portfolio/card3.jpg">
+            <h4>2017/11/17</h4>
             <p>【海外訂購服務開啟】</p>
           </a>
         </div>
@@ -33,8 +22,8 @@
       <div class="col-sm-4">
         <div class="card wow fadeInUp">
           <a href="">
-            <img src="https://www.teasoap.com.tw/downloads/bbs/pic/20160308131907.jpg" alt="Sit Amet Website" data-at2x="static/images/portfolio/card4.jpg">
-            <h3>2017/12/31</h3>
+            <img src="http://www.monga.com.tw/NewsImgs/79/3.jpg" alt="Sit Amet Website" data-at2x="static/images/portfolio/card4.jpg">
+            <h4>2017/12/31</h4>
             <p>【12/31～01/02暫停退貨退款作業】</p>
           </a>
         </div>
