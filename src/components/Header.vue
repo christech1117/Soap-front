7a15db569c4f1840c82dd1a5da21f753d46ce985
username <user email>
2017-05-05 10:33:15 +0800

    巡覽列完成

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,39 +1,30 @@
-<template lang="html">
+<template>
   <div class="header">
-
-    <!-- Top menu -->
-    <nav class="navbar navbar-fixed-top" role="navigation">
+    <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
       <div class="container">
+
+        <!--漢堡-->
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar-1">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#top-navbar">
             <span class="sr-only">Toggle navigation</span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
+          <!--品牌logo-->
           <a class="navbar-brand" href="http://localhost:8080/">Brand</a>
         </div>
-        <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="top-navbar-1">
+
+
+        <!--巡覽列-->
+        <div class="collapse navbar-collapse" id="top-navbar">
           <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a href="http://localhost:8080/news">最新消息</a>
-            </li>
-            <li>
-              <a href="http://localhost:8080/productMenu/List">商品介紹</a>
-            </li>
-            <li>
-              <a href="http://localhost:8080/rule">購物須知</a>
-            </li>
-            <li>
-              <a href="http://localhost:8080/about">關於我們</a>
-            </li>
-            <li>
-              <a href="http://localhost:8080/contact">聯絡我們</a>
-            </li>
-            <li>
-              <a href="http://localhost:8080/login">會員登入</a>
-            </li>
+            <li><a href="http://localhost:8080/news">最新消息</a></li>
+            <li><a href="http://localhost:8080/productMenu/List">商品介紹</a></li>
+            <li><a href="http://localhost:8080/rule">購物須知</a></li>
+            <li><a href="http://localhost:8080/about">關於我們</a></li>
+            <li><a href="http://localhost:8080/contact">聯絡我們</a></li>
+            <li><a href="http://localhost:8080/login">會員登入</a></li>
           </ul>
         </div>
       </div>
