ac168bcc88ee941a207ecaf611863cff262050b0
C:\xampp\htdocs <soap>
2017-05-09 01:11:43 +0800

    vue-bind

--- src/components/Product/ProductList.vue
+++ src/components/Product/ProductList.vue
@@ -1,113 +1,11 @@
 <template>
-  <div class="productList">
-    <div class="banner-img" style="background-image: url('http://demo.mor-e.com.tw/demo/yatia/images/main/6b149f1fdc0c0f8e2cc4594a60ec0b03.jpg')"></div>
-    <div class="container">
-      <div class="section-header">
-        <h1><i class="fa fa-th-list" aria-hidden="true"></i>商品列表</h1>
-      </div>
-      <ol class="breadcrumb">
-        <li><router-link :to="{ path: '/' }"><i class="fa fa-home" aria-hidden="true"></i></router-link></li>
-        <li class="active">商品列表</li>
-      </ol>
-      <div class="row">
-        <div class="col-md-3">
-          <ul class="">
-            <li><h3>產品展示</h3></li>
-            <li><router-link :to="{ path: '/productList' }">特價商品</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">熱銷商品</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">各種膚質</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">中油性膚質</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">混合性膚質</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">乾性膚質</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">嬰兒膚質</router-link></li>
-            <li><router-link :to="{ path: '/productList' }">敏感性膚質</router-link></li>
-          </ul>
-        </div>
-        <div class="col-md-9">
-          <div class="row">
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/139/20161115173412550_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/97/20151005105111382_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/98/20151005105302386_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/173/20170309103428686_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/174/20170309104857690_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/174/20170309104857690_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/175/20170309110151694_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/176/20170309110826698_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/177/20170309111803702_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/178/20170309112127706_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/179/20170309112712710_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/180/20170309112909714_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/182/20170309113517722_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/188/20170309133850746_bp.jpg" alt="">
-              </router-link>
-            </div>
-            <div class="col-xs-4 col-sm-3 col-md-3">
-              <router-link class="thumbnail" :to="{ path: '/productList/singleItem' }">
-                <img src="https://www.teasoap.com.tw/downloads/product/10000/1000/185/20170309114851734_bp.jpg" alt="">
-              </router-link>
-            </div>
-          </div>
-        </div>
-      </div>
+  <div class="productList" v-bind:style="bg_css">
+    <div class="cover">
+      <h3>{{ product_data.name }}</h3>
+      <h3>{{ product_data.price }}</h3>
+    </div>
+    <div class="info">
+      <p>{{ bg_css }}</p>
     </div>
   </div>
 </template>
@@ -115,35 +13,12 @@
 <script>
 
 export default {
-  data () {
-    return {
-      products: [
-        {
-          name: '生活調理乳',
-          price: 950,
-          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/174/20170309104857690_bp.jpg'
-        },
-        {
-          name: '生活保濕乳',
-          price: 900,
-          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg'
-        },
-        {
-          name: '潔',
-          price: 140,
-          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg'
-        },
-        {
-          name: '源',
-          price: 140,
-          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg'
-        },
-        {
-          name: '綠茶皂',
-          price: 180,
-          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg'
-        }
-      ]
+  props: ['product_data'],
+  computed: {
+    bg_css () {
+      return {
+        'background-image': "url('" + this.product_data.cover + "')"
+      }
     }
   }
 }
@@ -151,7 +26,8 @@ export default {
 
 <style scoped>
 .cover {
-  height: 300px;
-  background: no-repeat center
+  height: 200px;
+  background: no-repeat productList;
+  background-image: cover;
 }
 </style>
--- src/components/Product/index.vue
+++ src/components/Product/index.vue
@@ -1,14 +1,51 @@
 <template>
-
+  <div class="container">
+    <div class="col-sm-3" v-for="product in products">
+      <ProductList v-bind:product_data="product"></ProductList>
+    </div>
+  </div>
 </template>
 
 <script>
 import ProductList from './ProductList'
 
 export default {
-  components: { ProductList }
+  components: { ProductList },
+  data () {
+    return {
+      products: [
+        {
+          name: '生活調理乳',
+          price: 950,
+          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/174/20170309104857690_bp.jpg'
+        },
+        {
+          name: '生活保濕乳',
+          price: 900,
+          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/138/20161115173455546_bp.jpg'
+        },
+        {
+          name: '潔',
+          price: 140,
+          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/182/20170309113517722_bp.jpg'
+        },
+        {
+          name: '源',
+          price: 140,
+          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/184/20170309114104730_bp.jpg'
+        },
+        {
+          name: '綠茶皂',
+          price: 180,
+          cover: 'https://www.teasoap.com.tw/downloads/product/10000/1000/187/20170309132421742_bp.jpg'
+        }
+      ]
+    }
+  }
 }
 </script>
 <style scoped>
-
+.container {
+  margin-top: 70px
+}
 </style>
